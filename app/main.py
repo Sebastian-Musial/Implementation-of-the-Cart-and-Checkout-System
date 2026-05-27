e3b9e65877847be20cb664a71ab141165e86eeef
Sebastian Musial <dev.sebas.musial@gmail.com>
2026-05-28 01:40:04 +0200

    Create API endpoints for clients

--- app/main.py
+++ app/main.py
@@ -1,16 +1,18 @@
 from fastapi import FastAPI
 
 from app.database import create_db_and_tables
-from app.routers.products import router
+from app.routers.products import router as products_router
+from app.routers.clients import router as clients_router
 
-def create_app(create_tables_on_startup: bool = True) -> FastAPI:  #False w celu szybszego uruchamiania aplikacji
+def create_app(create_tables_on_startup: bool = False) -> FastAPI:  #False w celu szybszego uruchamiania aplikacji
     application = FastAPI()
 
     @application.get("/health", tags=["system"])
     def health_check() -> dict[str, str]:
         return {"status": "ok"}
 
-    application.include_router(router)
+    application.include_router(products_router)
+    application.include_router(clients_router)
 
     if create_tables_on_startup:
         create_db_and_tables()
--- app/schemas.py
+++ app/schemas.py
@@ -1,7 +1,7 @@
 from sqlmodel import Field, SQLModel
 from decimal import Decimal
 
-
+# Product
 class ProductCreate(SQLModel):
     name: str = Field(min_length=1, max_length=80)
     unit_price: Decimal = Field(max_digits=6, gt=0, decimal_places=3)
@@ -15,3 +15,15 @@ class ProductRead(SQLModel):
 
 class ProductChangeStock(SQLModel):
     amount_in_stock : int = Field (ge=0)
+
+# Client
+class ClientCreate(SQLModel):
+    name: str = Field(min_length=1, max_length=80)
+    surname: str = Field(min_length=1, max_length=80)
+    phone_number: str = Field(min_length=6, max_length=26)
+
+class ClientRead(SQLModel):
+    client_id: int
+    name: str
+    surname: str
+    phone_number: str
